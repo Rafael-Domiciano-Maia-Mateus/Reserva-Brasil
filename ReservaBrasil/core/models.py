17544952437355e6881033ab95e199c89af467ec
Rafael-Domiciano-Maia-Mateus <rmaia7985@gmail.com>
2025-06-23 20:01:18 -0300

    atualizacao

--- ReservaBrasil/core/models.py
+++ ReservaBrasil/core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 
-# Create your models here.
 class UserApp(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phoneNumber = models.CharField(max_length=20)
@@ -17,20 +15,21 @@ class Category(models.Model):
 
     def __str__(self):
         return str(self.name)
-    
+
 
 class Type(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return str(self.name)
-    
+
 
 class Property(models.Model):
     image = models.ImageField(null=True, blank=True)
     name = models.CharField(max_length=200)
     id_property = models.AutoField(primary_key=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     active = models.BooleanField(default=True)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     typeProperty = models.ForeignKey(Type, null=True, blank=True, on_delete=models.SET_NULL)
@@ -41,4 +40,20 @@ class Property(models.Model):
 
 
 class Reservation(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.ForeignKey(UserApp, on_delete=models.SET_NULL, null=True)
+    nameProperty = models.ForeignKey(Property, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    typeProperty = models.ForeignKey(Type, null=True, blank=True, on_delete=models.SET_NULL)
+    checkin = models.DateField()
+    checkout = models.DateField()
+    id_reservation = models.AutoField(primary_key=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rate = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+    def priceTotal(self):
+        diarias = (self.checkout - self.checkin).days
+        return (self.price or 0) * diarias + (self.rate or 0)
+
+    def __str__(self):
+        return f'Nome: {self.user.username}; id Usuário: {self.id_user}; Nome do Imóvel: {self.nameProperty}; id Reserva: {self.id_reservation}'
